896af94933f67269d52252b283cc7489f9e914e5
RJ2607 <rishijain2607@gmail.com>
2024-01-03 11:52:09 +0530

    Refactor HomePage layout and add new features

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:football_app/components/Bottom_nav.dart';
+
+// import '../components/Bottom_nav.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
@@ -14,9 +17,119 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: MyBottomNav(),
-        body: Row(
-          children: [Text("data", style: TextStyle(color: Colors.white))],
-        ));
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 0.05 * MediaQuery.of(context).size.height,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Home",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500)),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                      iconSize: 30,
+                      color: Colors.grey,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.notifications),
+                      iconSize: 30,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 13),
+              child: Container(
+                height: 200,
+                width: 400,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 15, left: 20, right: 20, bottom: 15),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 28,
+                        width: 105,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                            child: Text(
+                          "⚽ Top News",
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        )),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "ldsvvcjshvchjshcshjcjhsvhjhdvjhsvjchvsjvcdshjsvcjhsdcjscjsc",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    // color: Colors.red,
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: <Color>[
+                          Color.fromARGB(255, 118, 102, 237),
+                          Color.fromARGB(255, 13, 0, 188)
+                        ]),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Popular Teams",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 23,
+                      fontWeight: FontWeight.w600),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "VIEW ALL",
+                      style: TextStyle(fontSize: 10, color: Colors.grey),
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
