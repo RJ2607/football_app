a01a3ebbf56ec2e5276865fe73eed2e94034e6ee
RJ2607 <rishijain2607@gmail.com>
2024-01-05 11:52:33 +0530

    Refactor code to improve performance and readability

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: sort_child_properties_last, prefer_const_constructors
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:football_app/screens/Matches.dart';
+import 'package:football_app/screens/News.dart';
 
 // import '../components/Bottom_nav.dart';
 
@@ -19,45 +21,45 @@ class _MyHomeState extends State<MyHome> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 0.05 * MediaQuery.of(context).size.height,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Home",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w500)),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.search),
-                      iconSize: 30,
-                      color: Colors.grey,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.notifications),
-                      iconSize: 30,
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 0.05 * MediaQuery.of(context).size.height,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Home",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500)),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.search),
+                        iconSize: 30,
+                        color: Colors.grey,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.settings),
+                        iconSize: 30,
+                        color: Colors.grey,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
                 padding: const EdgeInsets.only(right: 13),
                 child: Container(
                   height: 200,
@@ -105,52 +107,202 @@ class _MyHomeState extends State<MyHome> {
                       borderRadius: BorderRadius.circular(15)),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Popular Teams",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 23,
-                      fontWeight: FontWeight.w600),
-                ),
-                TextButton(
-                    onPressed: () {},
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Popular Teams",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "VIEW ALL",
+                        style: TextStyle(fontSize: 10, color: Colors.grey),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              popularTeams(),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Upcoming Matches",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Matches()));
+                    },
                     child: Text(
                       "VIEW ALL",
                       style: TextStyle(fontSize: 10, color: Colors.grey),
-                    ))
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    height: 85,
-                    width: 85,
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(119, 83, 78, 78),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Center(
-                        child: Image.network(
-                      'https://media.api-sports.io/football/teams/541.png',
-                      height: 60,
-                    )),
-                  ),
+                    ),
+                  )
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 12,
+              ),
+              upcomingMatches(),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Latest News",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => News()));
+                    },
+                    child: Text(
+                      "VIEW ALL",
+                      style: TextStyle(fontSize: 10, color: Colors.grey),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  SingleChildScrollView popularTeams() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            height: 85,
+            width: 85,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(119, 83, 78, 78),
+                borderRadius: BorderRadius.circular(15)),
+            child: Center(
+                child: Image.network(
+              'https://media.api-sports.io/football/teams/541.png',
+              height: 60,
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SingleChildScrollView upcomingMatches() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            height: 195,
+            width: 140,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 7,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 5, bottom: 5, left: 10, right: 10),
+                    child: Text(
+                      "Old Trafford",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 17, 2, 230)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      'https://media.api-sports.io/football/teams/541.png',
+                      height: 62,
+                    ),
+                    Image.network(
+                      'https://media.api-sports.io/football/teams/541.png',
+                      height: 62,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Real Madrid",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "VS",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "FC Barcelona",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500),
+                    )
+                  ],
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Color.fromARGB(255, 103, 84, 245),
+                      Color.fromARGB(255, 17, 2, 230)
+                    ]),
+                borderRadius: BorderRadius.circular(15)),
+          ),
+        ],
       ),
     );
   }
