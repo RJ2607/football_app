a3d46c7feff59a23058d0c6dd627ac87dd8c21db
RJ2607 <rishijain2607@gmail.com>
2024-01-07 20:39:27 +0530

    Refactor API call and add testPage

--- lib/Api manger/team data/team_data.dart
+++ lib/Api manger/team data/team_data.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:football_app/Api%20manger/team%20data/team_model.dart';
 import 'package:http/http.dart' as http;
-
 
 class TeamsAPI {
   final String apiURL = 'https://v3.football.api-sports.io/teams';
@@ -12,16 +13,23 @@ class TeamsAPI {
     'x-rapidapi-key': api_Key
   };
 
-  // Future<List<TeamModel>> getAllTeams() async {
-  //   final response = await http.get(Uri.parse(apiURL), headers: headers);
+  void getAllTeams() async {
+    final response = await http.get(Uri.parse(apiURL), headers: headers);
 
-  //   if (response.statusCode == 200) {
-  //     final Map<String, dynamic> teamsData = json.decode(response.body);
-  //     final List<dynamic> teamsList = teamsData['response'];
+    if (response.statusCode == 200) {
+      final teamsData = jsonDecode(response.body);
+      final List<dynamic> result = teamsData['response'];
 
-  //     return teamsList.map((json) => TeamModel.fromJson(json)).toList();
-  //   } else {
-  //     throw Exception('Failed to load data!');
-  //   }
-  // }
+      final teamList = result.map((e) {
+        return TeamModel(
+          // id: e['team']['id'],
+          code: e['code'],
+          name: e['name'],
+          logo: e['logo'],
+        );
+      }).toList();
+    } else {
+      throw Exception('Failed to load data!');
+    }
+  }
 }
--- lib/Api manger/team data/team_model.dart
+++ lib/Api manger/team data/team_model.dart
@@ -1,22 +1,13 @@
 class TeamModel {
-  final int id;
+  // final int id;
   final String code;
   final String name;
   final String logo;
 
   TeamModel({
-    required this.id,
+    // required this.id,
     required this.code,
     required this.name,
     required this.logo
   });
-
-  factory TeamModel.fromJson(Map<String, dynamic> json) {
-    return TeamModel(
-      id: json['id'],
-      code: json['code'],
-      name: json['name'],
-      logo: json['logo']
-    );
-  }
 }
