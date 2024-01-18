7fd4b294a888c4f0aa122c76fd0b6a0a32ee120a
RJ2607 <rishijain2607@gmail.com>
2024-01-19 00:28:41 +0530

    Update x-rapidapi-key in ApiFunction class

--- lib/Api/api_function.dart
+++ lib/Api/api_function.dart
@@ -6,7 +6,8 @@ class ApiFunction {
   Future<Map<String, dynamic>> standingsDatabyidApi(String id) async {
     var headers = {
       'x-rapidapi-host': 'v3.football.api-sports.io',
-      'x-rapidapi-key': 'd0efa14cef0dffff3d72504c72d5ccef'
+      'x-rapidapi-key':
+          'c8964bcd76b89bf9276e0d3a7c8fa9ac' //'d0efa14cef0dffff3d72504c72d5ccef'
     };
     var request = http.Request(
         'GET',
